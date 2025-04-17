2769186fdecbc1981a94924192e24443a7399f7c
Yordin Da Rocha <y.alayn@gmail.com>
2025-04-16 21:32:41 -0400

    Fix: Eliminar token

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_CONFIG } from '../config/apiConfig';
 
 // Simulación de obtención de token (ajusta con AsyncStorage o contexto real)
-let accessToken: string | null = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTA1NzgsInVzZXJuYW1lIjoieS5hbGF5bkBnbWFpbC5jb20iLCJpYXQiOjE3NDQ4MDQ4MTgsImV4cCI6MTc0NDg5MTIxOH0.jFN7ne_cwxNZXEw649NVEeZUce8EA_xWaGX9KMdi260";
+let accessToken: string | null = "";
 
 export const setAccessToken = (token: string) => {
   accessToken = token;
