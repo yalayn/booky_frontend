6b9a44a21663c0a5e2126d2bcb72fb857a7cda30
Yordin Da Rocha <y.alayn@gmail.com>
2025-04-21 22:25:33 -0400

    Feat: Ajuste de configuraciones globales

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import Toast, { BaseToast } from 'react-native-toast-message';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './src/screens/HomeScreen';
@@ -8,6 +9,24 @@ import LibraryScreen from './src/screens/LibraryScreen';
 import SearchScreen from './src/screens/SearchScreen';
 
 const Stack = createNativeStackNavigator();
+
+const configToast = {
+  success: (props) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: 'green' }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{
+        fontSize: 15,
+        fontWeight: 'bold',
+      }}
+      text2Style={{
+        fontSize: 13,
+        color: 'gray',
+      }}
+    />
+  ),
+};
 
 function App(): React.JSX.Element {
   return (
@@ -35,6 +54,7 @@ function App(): React.JSX.Element {
             options={{ title: 'Detalle del Libro' }}
           />
         </Stack.Navigator>
+        <Toast config={configToast} />
       </NavigationContainer>
     </SafeAreaProvider>
   );
