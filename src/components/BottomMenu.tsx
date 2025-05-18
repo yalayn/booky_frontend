8e5670242604f0d151f7683ebf1f69649ec3e813
Yordin Da Rocha <y.alayn@gmail.com>
2025-05-18 14:25:24 -0400

    Fix: Ajustes visuales

--- src/components/BottomMenu.tsx
+++ src/components/BottomMenu.tsx
@@ -5,45 +5,7 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import { Colors } from "../styles/AppStyles";
 
 const BottomMenu = ({navigation, currentView}) => {
-  const listViews_old = {
-    "home":{
-      "Library":{
-        icon: "book",
-        label: "Biblioteca",
-        navigateTo: "Library"
-      }, 
-      "Search":{
-        icon: "search",
-        label: "Agregar",
-        navigateTo: "Search"
-      }
-    },
-    "library":{
-      "Home":{
-        icon: "home",
-        label: "Inicio",
-        navigateTo: "Home"
-      },
-      "Search":{
-        icon: "search",
-        label: "Agregar",
-        navigateTo: "Search"
-      }
-    },
-    "search":{
-      "Home":{
-        icon: "home",
-        label: "Inicio",
-        navigateTo: "Home"
-      },
-      "Library":{
-        icon: "book",
-        label: "Biblioteca",
-        navigateTo: "Library"
-      }
-    },
-  };
-
+  
   const listViews = {
     "home":{
       icon      : "home",
@@ -69,7 +31,6 @@ const BottomMenu = ({navigation, currentView}) => {
             style={[StyleBottomMenu.menuButton, currentView === key && { backgroundColor: Colors.primary}]}
             onPress={() => navigation.navigate(navigateTo)}
           >
-            {/* <Icon name={icon} size={24} color={Colors.lighter} /> */}
             <Text style={StyleBottomMenu.menuButtonText} > {label} </Text>
           </TouchableOpacity>
         ))}
@@ -80,21 +41,20 @@ const BottomMenu = ({navigation, currentView}) => {
 const StyleBottomMenu = StyleSheet.create({
   bottomMenu: {
     position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
+    bottom: 24,
+    left: 56,
+    right: 56,
     flexDirection: "row",
     justifyContent: "space-around",
     backgroundColor: Colors.darker,
-    borderTopWidth: 1,
-    borderTopColor: "#E0E0E0",
-    paddingVertical: 12,
+    paddingVertical: 6,
+    borderRadius: 20,
   },
   menuButton: {
     alignItems: "center",
-    backgroundColor: Colors.darker,
-    borderRadius: 8,
-    padding: 8,
+    borderRadius: 16,
+    paddingVertical: 4,
+    paddingHorizontal: 10,
   },
   menuButtonText: {
     color: Colors.lighter,
