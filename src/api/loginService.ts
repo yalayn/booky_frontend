e8cea012b5a76654ef906cf6689687e71a0c214a
Yordin Da Rocha <y.alayn@gmail.com>
2025-05-26 21:20:30 -0400

    Feat: Incorpora registro de usuario

--- src/api/loginService.ts
+++ src/api/loginService.ts
@@ -1,7 +1,22 @@
 // src/api/bookService.ts
 import httpClient from './httpClient';
 
+/**
+ * Validates the user credentials and initializes the login process.
+ * @param jsonParams 
+ * @returns 
+ */
 export const initLogin = async (jsonParams: any) => {
   const response = await httpClient.post('/users/login', jsonParams);
+  return response.data;
+}
+
+/**
+ * Registers a new user with the provided parameters.
+ * @param jsonParams 
+ * @returns 
+ */
+export const addUser = async (jsonParams: any) => {
+  const response = await httpClient.post('/users/register', jsonParams);
   return response.data;
 }
