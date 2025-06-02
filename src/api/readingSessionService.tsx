b72e08f9b25afe9da185fe69c51c9c8d73284e43
Yordin Da Rocha <y.alayn@gmail.com>
2025-06-01 20:04:31 -0400

    Feat: Se crea servicio de listado de registro de lectura

--- src/api/readingSessionService.tsx
+++ src/api/readingSessionService.tsx
@@ -1,7 +1,7 @@
 import httpClient from './httpClient';
 
 export const getReadingSessionsToday = async () => {
-  const response = await httpClient.get('/reading-sessions/user_count_day');
+  const response = await httpClient.get('/reading-sessions/today');
   return response.data;
 };
 
@@ -9,3 +9,8 @@ export const registerReadingSessions = async (jsonParams:any) => {
   const response = await httpClient.post('/reading-sessions/register',jsonParams);
   return response.data;
 };
+
+export const getReadingSessionsHistory = async () => {
+  const response = await httpClient.get('/reading-sessions/history');
+  return response.data;
+}
