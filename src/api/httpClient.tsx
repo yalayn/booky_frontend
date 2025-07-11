21336fd260937b69b7720b950ed4b2e1d83a52f5
Yordin Da Rocha <y.alayn@gmail.com>
2025-07-11 19:49:54 -0400

    Feat: Se incorpora refresco de token en interceptor

--- src/api/httpClient.tsx
+++ src/api/httpClient.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { API_CONFIG } from '../config/apiConfig';
+import { initRefreshToken } from '../api/loginService'; // Asegúrate de que esta ruta sea correcta
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Simulación de obtención de token (ajusta con AsyncStorage o contexto real)
 let accessToken: string | null;
@@ -17,14 +19,45 @@ const httpClient = axios.create({
 });
 
 // Interceptor para agregar el token a cada request
-httpClient.interceptors.request.use(
-  async (config) => {
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
+// httpClient.interceptors.request.use(
+//   async (config) => {
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+httpClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    // Si el error es de autenticación, intenta refrescar el token
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        // Aquí deberías implementar la lógica para refrescar el token
+        // Por ejemplo, llamando a un servicio de refresh token
+        const refreshToken = await AsyncStorage.getItem('refreshToken');
+        if (!refreshToken) {
+          throw new Error('No se pudo obtener el refresh token');
+        }
+        const response = await initRefreshToken(refreshToken);
+        if (!response) {
+          throw new Error('No se pudo obtener un nuevo access token');
+        }
+        const newAccessToken  = response.data.token;
+        setAccessToken(newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return httpClient(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
     }
-    return config;
-  },
-  (error) => Promise.reject(error)
+
+    return Promise.reject(error);
+  }
 );
 
 export default httpClient;
