27e94c773dd798b0a04ac61299448bbc25f034b9
Yordin Da Rocha <y.alayn@gmail.com>
2025-07-14 21:02:04 -0400

    Fix: Ajuste enviroment para pruebas produccion

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,17 +1,22 @@
 // src/config/apiConfig.ts
 
 const ENV = {
-    development: {
-      API_BASE_URL: 'http://localhost:3005/api',
-    },
-    production: {
-      API_BASE_URL: 'https://api.tuapi.com/api',
-    },
-  };
-  
-  const getEnv = (): keyof typeof ENV => {
-    // Puedes mejorar esto usando una variable de entorno real
-    return __DEV__ ? 'development' : 'production';
-  };
-  
-  export const API_CONFIG = ENV[getEnv()];
+  development: {
+    API_BASE_URL: 'http://localhost:3005/api',
+  },
+  production: {
+    API_BASE_URL: 'https://booky-backend-node.onrender.com/api',
+  },
+};
+
+const getEnv = (): keyof typeof ENV => {
+  // Usa la variable de entorno real
+  // const env = process.env.NODE_ENV;
+  const env = 'production'; // For testing purposes, we set it to production
+  // const env = 'development'; // For testing purposes, we set it to development
+  console.log('Current environment:', env);
+  // if (env === 'development') return 'development';
+  return env;
+};
+
+export const API_CONFIG = ENV[getEnv()];
